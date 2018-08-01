0f065ea7c6900df0fce516da48fdf71f1a35d552
bkrigel <benjamin.krigel@gmail.com>
2018-08-01 16:58:46 -0400

    added email:string and password_digest:string to both the Doctors and Patients migrations.

--- db/migrate/20180801200720_create_doctors.rb
+++ db/migrate/20180801200720_create_doctors.rb
@@ -1,6 +1,8 @@
 class CreateDoctors < ActiveRecord::Migration[5.2]
   def change
     create_table :doctors do |t|
+      t.string :email
+      t.string :password_digest
       t.string :first_name
       t.string :last_name
       t.string :location_city
--- db/migrate/20180801200831_create_patients.rb
+++ db/migrate/20180801200831_create_patients.rb
@@ -1,6 +1,8 @@
 class CreatePatients < ActiveRecord::Migration[5.2]
   def change
     create_table :patients do |t|
+      t.string :email
+      t.string :password_digest
       t.string :first_name
       t.string :last_name
       t.string :ailment
