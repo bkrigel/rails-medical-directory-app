00d4908f92de0ab5b757ac6790d6abc1a1e0a32a
bkrigel <benjamin.krigel@gmail.com>
2018-08-01 16:39:56 -0400

    added a StaticPages controller with a home view, a Sessions controller with a new view, and routes for both.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :appointments
   resources :doctors
   resources :specialties
+  root 'static_pages#home'
+  get '/signin', to: 'session#new', as: 'signin'
+  post '/session', to: 'session#create', as: 'session'
+  delete '/session/', to: 'session#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
