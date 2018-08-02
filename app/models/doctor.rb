37e09dbfbe07cdcc936abae4252869a283bd5fa3
bkrigel <benjamin.krigel@gmail.com>
2018-08-02 17:58:41 -0400

    added full_name methods for Patient and Doctor. Also, set up Doctors show page.

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,9 @@ class Doctor < ApplicationRecord
   belongs_to :specialty, optional: true
   has_many :appointments
   has_many :patients, through: :appointments
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+  
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,9 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :doctors, through: :appointments
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
