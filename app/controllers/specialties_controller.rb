aef49072492571517082a262d7830e4d480385c6
bkrigel <benjamin.krigel@gmail.com>
2018-08-03 12:27:47 -0400

    added index and show pages for specialties

--- app/controllers/specialties_controller.rb
+++ app/controllers/specialties_controller.rb
@@ -1,5 +1,15 @@
 class SpecialtiesController < ApplicationController
   def index
+    @specialties = Specialty.all
+  end
 
+  def show
+    @specialty = Specialty.find_by(id: params[:id])
+    @doctors = []
+    Doctor.all.select do |doctor|
+      if doctor.specialty_id == @specialty.id
+        @doctors << doctor
+      end
+    end
   end
 end
