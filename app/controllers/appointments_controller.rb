6487ba9e6d495efbc5f0fe22f5a51658e643f643
bkrigel <benjamin.krigel@gmail.com>
2018-08-06 09:49:26 -0400

    added logic in controllers to prevent unauthorized users from viewing patients and appointments show pages. also added new bullet point to doctors profile to view count of patients

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,6 +3,9 @@ class AppointmentsController < ApplicationController
   def show
     @appointment = Appointment.find_by(id: params[:id])
     @doctor = @appointment.doctor
+    unless current_user.role == @appointment.patient || current_user.role == @appointment.doctor
+      redirect_to root_path
+    end
   end
 
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,10 +3,9 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find_by_id(params[:id])
     @doctors = @patient.doctors
-    if current_user.role == @patient || @doctors.include?(current_user.role)
-      @ailments = @patient.ailments
-      @appointments = @patient.appointments
-    else
+    @ailments = @patient.ailments
+    @appointments = @patient.appointments
+    unless current_user.role == @patient || @doctors.include?(current_user.role)
       redirect_to root_path
     end
   end
