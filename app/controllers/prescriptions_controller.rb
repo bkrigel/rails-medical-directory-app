66c2c8082527c6672e6e3a3e2fd23549b9aa6bb6
bkrigel <benjamin.krigel@gmail.com>
2018-08-06 17:32:29 -0400

    finalized prescriptions#create action

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -4,6 +4,18 @@ class PrescriptionsController < ApplicationController
     @prescription = Prescription.new
   end
 
+  def create
+    prescription = Prescription.new(drug: params[:prescription][:drug],
+                    dosage_in_milligrams: params[:prescription][:dosage_in_milligrams],
+                    appointment_id: params[:prescription][:appointment]
+                  )
+    if prescription.save
+      redirect_to specialty_doctor_path(prescription.appointment.doctor.specialty, prescription.appointment.doctor)
+    else
+      redirect_to new_prescription_path
+    end
+  end
+
   def show
     @prescription = Prescription.find_by(id: params[:id])
     @appointment = @prescription.appointment
