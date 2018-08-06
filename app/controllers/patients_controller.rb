70e5d3f2790ffcff78f5b194083799d1fbaadf24
bkrigel <benjamin.krigel@gmail.com>
2018-08-06 09:18:44 -0400

    added conditional to view Patient show page only if the current user is the patient or one of the patients doctors

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,12 +3,12 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find_by_id(params[:id])
     @doctors = @patient.doctors
-    # if current_user.role == @patient || current_user.role == @doctors.any?
+    if current_user.role == @patient || @doctors.include?(current_user.role)
       @ailments = @patient.ailments
       @appointments = @patient.appointments
-    # else
-    #   redirect_to root_path
-    # end
+    else
+      redirect_to root_path
+    end
   end
 
 end
