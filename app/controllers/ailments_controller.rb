6b4aba37055ab6c83f824cd6392d401d643e2abb
bkrigel <benjamin.krigel@gmail.com>
2018-08-06 22:33:57 -0400

    finished create new Ailment form and actions

--- app/controllers/ailments_controller.rb
+++ app/controllers/ailments_controller.rb
@@ -4,4 +4,17 @@ class AilmentsController < ApplicationController
     @ailment = Ailment.new
   end
 
+  def create
+    ailment = Ailment.new(
+      first_noticed_on: params[:ailment][:first_noticed_on],
+      description: params[:ailment][:description],
+      patient_id: current_user.role.id
+    )
+    if ailment.save
+      redirect_to patient_path(ailment.patient)
+    else
+      redirect_to new_ailment_path
+    end
+  end
+
 end
--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -5,12 +5,14 @@ class PrescriptionsController < ApplicationController
   end
 
   def create
-    prescription = Prescription.new(drug: params[:prescription][:drug],
-                    dosage_in_milligrams: params[:prescription][:dosage_in_milligrams],
-                    appointment_id: params[:prescription][:appointment]
-                  )
+    prescription = Prescription.new(
+      drug: params[:prescription][:drug],
+      dosage_in_milligrams: params[:prescription][:dosage_in_milligrams],
+      appointment_id: params[:prescription][:appointment]
+    )
     if prescription.save
-      redirect_to specialty_doctor_path(prescription.appointment.doctor.specialty, prescription.appointment.doctor)
+      redirect_to specialty_doctor_path(prescription.appointment.doctor.specialty,
+                            prescription.appointment.doctor)
     else
       redirect_to new_prescription_path
     end
