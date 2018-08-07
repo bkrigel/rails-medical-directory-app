0561762e768618f138dc18174c26de66bf6c0421
bkrigel <benjamin.krigel@gmail.com>
2018-08-07 00:08:28 -0400

    finished new Appointment form and controller create action

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,6 +4,21 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.new
   end
 
+  def create
+    appointment = Appointment.new(
+      scheduled_for: DateTime.strptime("#{params[:appointment][:scheduled_for]} #{params[:appointment]["scheduled_for(4i)"]}:#{params[:appointment]["scheduled_for(5i)"]}", "%Y-%m-%d %H:%M"),
+      duration_in_minutes: params[:appointment][:duration_in_minutes],
+      doctor_id: params[:appointment][:doctor],
+      ailment_id: params[:appointment][:ailment],
+      patient_id: current_user.role.id
+    )
+    if appointment.save
+      redirect_to patient_path(appointment.patient)
+    else
+      redirect_to new_appointment_path
+    end
+  end
+
   def show
     @appointment = Appointment.find_by(id: params[:id])
     @doctor = @appointment.doctor
