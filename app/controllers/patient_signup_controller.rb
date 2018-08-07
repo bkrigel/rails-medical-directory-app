9b7fb653b5c83d28edcbdbace40b8bd9c97e7ca1
bkrigel <benjamin.krigel@gmail.com>
2018-08-07 12:11:10 -0400

    finished new patient/user signup form

--- app/controllers/patient_signup_controller.rb
+++ app/controllers/patient_signup_controller.rb
@@ -4,14 +4,18 @@ class PatientSignupController < ApplicationController
   end
 
   def create
-    user = User.new(email: params[:user][:email],
-                    password: params[:user][:password],
-                    password_confirmation: params[:user][:password_confirmation]
-                  )
-    user.role = Patient.create
-    if user.save
+    user = User.new(
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation]
+    )
+    user.role = Patient.new(
+      first_name: params[:user][:role_attributes][:first_name],
+      last_name: params[:user][:role_attributes][:last_name]
+    )
+    if user.save && user.role.save
       session[:user_id] = user.id
-      redirect_to specialties_path
+      redirect_to root_path
     else
       redirect_to patient_signup_path
     end
