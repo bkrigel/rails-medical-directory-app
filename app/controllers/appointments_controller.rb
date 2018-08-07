370a683166d60965d921139f95b1a8f7f1138eea
bkrigel <benjamin.krigel@gmail.com>
2018-08-07 00:55:51 -0400

    added edit functionality to Appointments and moved the new/edit forms into a partial

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -19,6 +19,25 @@ class AppointmentsController < ApplicationController
     end
   end
 
+  def edit
+    @appointment = Appointment.find_by(id: params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find_by(id: params[:id])
+    if @appointment.update(
+      scheduled_for: DateTime.strptime("#{params[:appointment][:scheduled_for]} #{params[:appointment]["scheduled_for(4i)"]}:#{params[:appointment]["scheduled_for(5i)"]}", "%Y-%m-%d %H:%M"),
+      duration_in_minutes: params[:appointment][:duration_in_minutes],
+      doctor_id: params[:appointment][:doctor],
+      ailment_id: params[:appointment][:ailment],
+      patient_id: current_user.role.id
+    )
+      redirect_to patient_path(@appointment.patient)
+    else
+      redirect_to edit_appointment_path(@appointment)
+    end
+  end
+
   def show
     @appointment = Appointment.find_by(id: params[:id])
     @doctor = @appointment.doctor
